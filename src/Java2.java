99c03ed0493c55c9333917b9a6d2e3eb2f6b368f
sheeqaaloow <maqane2016@outlook.com>
2024-10-23 19:27:13 -0700

    first commit

---
+++ src/Java2.java
@@ -0,0 +1,24 @@
+public class Java2 {
+    public static void main(String[] args) {
+//        int num = 10;
+//        int num2 =20;
+//        int num3 = num+num2;
+//        System.out.println(num3);
+//        String studentName = "John Doe";
+//        int studentID = 15;
+//        int studentAge = 23;
+//        float studentFee = 75.25f;
+//        char studentGrade = 'B';
+
+// Print variables
+//        System.out.println("Student name: " + studentName);
+//        System.out.println("Student id: " + studentID);
+//        System.out.println("Student age: " + studentAge);
+//        System.out.println("Student fee: " + studentFee);
+//        System.out.println("Student grade: " + studentGrade);
+
+
+String name = "cabdifitaax ";
+        System.out.println(name.toUpperCase());
+    }
+}
