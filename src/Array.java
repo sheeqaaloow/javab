ad878578c0e24bf2b35090397bd9f543ed8f14be
sheeqaaloow <maqane2016@outlook.com>
2024-11-01 20:08:46 -0700

    first commit

---
+++ src/Array.java
@@ -0,0 +1,60 @@
+public class Array {
+    public static void main(String[] args) {
+//        String[] cars = {"masda", "bmw", "volvo", "fiat", "toyota"};
+//        for (int i = 0; i < cars.length ; i++) {
+//            System.out.println(cars [2]);
+//        }
+//        int sum = 0;
+//        int[] numbers  = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//        for (int i = 0; i < numbers.length; i++) {
+//            sum += numbers[i];
+//
+//        };
+//        //System.out.println(sum);
+//        int length = numbers.length;
+//        int resolve=sum/length;
+//        System.out.println(resolve);
+//          String[] names = {"madey", "C/fitaax", "mahamed", "naasir"};
+//                  for (int i = 0; i < names.length; i++) {
+//
+//        }
+//        System.out.println(names.length);
+
+
+//int sum = 0;
+//
+//        for (int number : numbers) {
+//sum += number;
+//        }
+//
+////                System.out.println("Sum: " + sum);
+//            int sum = 0;
+//            int [] numbers = { 1, 2, 3, 4, 5,};
+//            for (int num : numbers){
+//                System.out.println(num);
+//                sum += num;
+//
+//
+//            }
+////            System.out.println(sum);
+//              int sum =0;
+//        for (int i = 0; i < 5; i++) {
+//            //System.out.println(i);
+//            sum+=i;
+//
+//
+//        }
+//        System.out.println(sum);
+        //int number = 10;
+        for (int i = 0; i < 10; i++) {
+
+
+            if (i % 2 == 0) {
+                System.out.println("Number is Even " + i);
+            } else {
+                System.out.println("Number is odd " + i);
+            }
+
+    }   } }
+
+
--- src/Java2.java
+++ src/Java2.java
@@ -1,48 +1,88 @@
-public class Java2 {
-    public static void main(String[] args) {
-//        int num = 10;
-//        int num2 =20;
-//        int num3 = num+num2;
-//        System.out.println(num3);
-//        String studentName = "John Doe";
-//        int studentID = 15;
-//        int studentAge = 23;
-//        float studentFee = 75.25f;
-//        char studentGrade = 'B';
-
-// Print variables
-//        System.out.println("Student name: " + studentName);
-
-//        System.out.println("Student id: " + studentID);
-
-//        System.out.println("Student age: " + studentAge);
-//        System.out.println("Student fee: " + studentFee);
-//        System.out.println("Student grade: " + studentGrade);
-
-
-//       int x =10;
-//       int y = 20;
-//        System.out.println(x < y)
-//      int age1 = 17;
-//      int age2 = 20;
-//        System.out.println(age1 > 18 );
-//        System.out.println(age2 > 18 );
-//        int  x = 10 ;
-//        int y = 18 ;
-//        if (x < y){
-//            System.out.println("x > y");
-
-            int time = 5;
-            if (time < 18) {
-                System.out.println("Good day.");
-            } else {
-                System.out.println("Good evening.");
-
-            }
-
-        }
-   }
-
-
-
-
+//public class Java2 {
+//    public static void main(String[] args) {
+////        int num = 10;
+////        int num2 =20;
+////        int num3 = num+num2;
+////        System.out.println(num3);
+////        String studentName = "John Doe";
+////        int studentID = 15;
+////        int studentAge = 23;
+////        float studentFee = 75.25f;
+////        char studentGrade = 'B';
+//
+//// Print variables
+////        System.out.println("Student name: " + studentName);
+//
+////        System.out.println("Student id: " + studentID);
+//
+////        System.out.println("Student age: " + studentAge);
+////        System.out.println("Student fee: " + studentFee);
+////        System.out.println("Student grade: " + studentGrade);
+//
+//
+////       int x =10;
+////       int y = 20;
+////        System.out.println(x < y)
+////      int age1 = 17;
+////      int age2 = 20;
+////        System.out.println(age1 > 18 );
+////        System.out.println(age2 > 18 );
+////        int  x = 10 ;
+////        int y = 18 ;
+////        if (x < y){
+////            System.out.println("x > y");
+//
+////            int time = 5;
+////            if (time < 18) {
+////                System.out.println("Good day.");
+////            } else {
+////                System.out.println("Good evening.");
+////        a. -5 + 8 * 6
+////        b. (55+9) % 9
+////        c. 20 + -3*5 / 8
+////        d. 5 + 15 / 3 * 2 - 8 % 3
+////          int a = -5;
+////         int b = 8;
+////          int c = 6;
+////         int z = a+(b*c);
+////        System.out.println(z);
+////          int a = 55;
+////          int  b = 9;
+////          int  c = 9;
+////          int z = (a+b) %c;
+////        System.out.println(z);
+////            int a = 5;
+////            int b = 15;
+////            int c = 3;
+////            int d = 2;
+////            int e = -8;
+////            int f = 3;
+////            // 5 + 15 / 3 * 2 - 8 % 3
+////            int g = a+b/c*d-e%f;
+////            System.out.println(c);
+//
+//        {
+//
+//        }
+//
+//            for (int i = 0; i < 10; i++) {
+//                if (i == 4) {
+//                    break;
+//                }
+//                System.out.println(i);
+//                for (int i = 0; i < 10; i++) {
+//                    if (i == 4) {
+//                        continue;
+//                    }
+//                    System.out.println(i);
+//                }
+//            } }
+//
+//        }
+//
+//
+//
+//
+//
+//
+//
